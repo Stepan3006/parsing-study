e91c4e6000508b8c7159d8fbecf114b49a9b6736
Stepan3006 <stepan@onepk.ru>
2022-11-09 21:11:44 +0300

    Add request sessions

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import fake_useragent
 from bs4 import BeautifulSoup
 
 s = requests.Session()
+user = fake_useragent.UserAgent(use_cache_server=False).random()
+
+header = {
+    'user-agent':user
+}
 data = {"login_username":"stepan@onepk.ru", "login_password":""}
 
 url = 'https://stepik.org/learn?auth=login'
 
-r = s.post(url, data=data)
+r = s.post(url, data=data, headers=header).text
 
 print(s.cookies)
 
