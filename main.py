f6e03e7c93923c579c6cdd73cc00733f3d0a0913
Stepan3006 <stepan@onepk.ru>
2022-11-09 21:29:59 +0300

    Maximum amount of retries reached

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-import requests
+import telebot
+
+import random
+
 import fake_useragent
-from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+ua = UserAgent()
 
-s = requests.Session()
-user = fake_useragent.UserAgent(use_cache_server=False).random()
+from faker import Faker
+fake = Faker('ru_RU')
+import faker
 
-header = {
-    'user-agent':user
-}
-data = {"login_username":"stepan@onepk.ru", "login_password":""}
+import requests
 
-url = 'https://stepik.org/learn?auth=login'
+fake_user = fake_useragent.UserAgent().random
 
-r = s.post(url, data=data, headers=header).text
+fake_name = fake.name()
+fake_password = fake.password()
+fake_email = fake.email()
 
-print(s.cookies)
+symbols = ['a','b''d','c','e','f','g','q','z','r','u','y','p']
+fake_username = random.choice(symbols) + str(fake.random.randint(10000000,100000000)) + random.choice(symbols)
 
-print(r)
+print(f'{fake_name} {fake_password} {fake_email} {fake_username}')
+
+url = 'https://knigavuhe.org/register/'
+
+user_agent_val = fake_user
+
+session = requests.Session()
+r = session.get(url, headers = {
+    'User-Agent': user_agent_val
+})
+
+session.headers.update({'Referer':url})
+
+session.headers.update({'User-Agent':user_agent_val})
+
+_xsrf = session.cookies.get('_xsrf', domain=".org")
+
+post_request = session.post(url, {
+     'backUrl': 'https://knigavuhe.org/',
+     'name': fake_name,
+     'surname': fake_name,
+     'username': fake_username,
+     'email': fake_email,
+     'password': fake_password,
+     '_xsrf':_xsrf,
+     'remember':'yes',
+})
